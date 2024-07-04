eb8cc161b5b648cb4384a31e48d32cf8ec1bd760
KimSeungDeok <sdkksd6331@gmail.com>
2024-07-04 15:24:04 +0900

    m7d4js3

---
+++ teltron/javascript/project4/tav.js
@@ -0,0 +1,50 @@
+var products = [
+  { id : 0, price : 70000, title : 'Blossom Dress' },
+  { id : 1, price : 50000, title : 'Springfield Shirt' },
+  { id : 2, price : 60000, title : 'Black Monastery' }
+];
+
+function 카드넣기 (x) {
+    document.querySelectorAll('.card-price')[x].innerHTML = '가격 : ' + products[x].price
+    document.querySelectorAll('.card-title')[x].innerHTML = products[x].title
+}
+
+for (i = 0; i < 3; i++) {
+    카드넣기(i)
+}
+
+var pants = [28, 30, 32, 34]
+var shirt = [100, 105, 110, 115, 120]
+
+$('.form-select').eq(0).on('input', function(){
+
+  var value = $('.form-select').eq(0).val();
+  if (value == '셔츠') {
+    $('.form-select').eq(1).removeClass('form-hide');
+    $('.form-select').eq(1).html('');
+    shirt.forEach(function(r){
+      $('.form-select').eq(1).append(`<option>${r}</option>`)
+    })
+  }
+  else if (value == '바지'){
+    $('.form-select').eq(1).removeClass('form-hide');
+    $('.form-select').eq(1).html('');
+    pants.forEach(function(e){
+      $('.form-select').eq(1).append(`<option>${e}</option>`)
+    })
+  }
+  else if (value == '모자'){
+    $('.form-select').eq(1).addClass('form-hide')
+  }
+
+});
+
+
+$.get('https://codingapple1.github.io/hello.txt')
+  .done(function(data){
+    console.log(data)
+  })
+  .fail(function(error){
+    console.log('실패함')
+  });
+
